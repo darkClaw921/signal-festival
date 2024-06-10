4d8842ec3caeb69cc20db6f8a65b28c23455e8f8
Igor Gerasimov <gerasimov.igor.98@icloud.com>
2024-06-10 19:03:54 +0300

    minor

--- signal_festival/workTelegram.py
+++ signal_festival/workTelegram.py
@@ -21,7 +21,7 @@ async def main():
     dp = Dispatcher(storage=MemoryStorage())
     dp.include_router(router)
     await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types(), ssl_context=False)
 
 
 
